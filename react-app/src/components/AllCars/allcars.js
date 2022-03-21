b37fd68f558ee2d5c49d92a73fbec1fd1ff21965
Sai Byri <saikiran169@gmail.com>
2022-03-20 22:29:04 -0700

    all cars change

--- react-app/src/components/AllCars/allcars.js
+++ react-app/src/components/AllCars/allcars.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect, useHistory } from 'react-router-dom';
+import { Link, Redirect, useHistory } from 'react-router-dom';
 import * as carActions from '../../store/car'
 
 export default function AllCars() {
@@ -16,19 +16,36 @@ export default function AllCars() {
     console.log('THIS IS CARS', cars);
     return (
         <div className='mainCarsContainer'>
-            <div className='eachCar'>
-                <div className='eachCarUl'>
+            <div className='all-cars-container-title'>
+                ALL VEHICLES
+                <div className='all-cars-main-grid'>
                     {cars?.map((car, i) => (
-                        <div className='eachCarli' key={`${i}`}>
-                            {car.year}
-                            {car.make}
-                            {car.model}
-                            {car.body_style}
-                            {car.fuel_type}
-                            {car.price}
-                            {car.ext_color}
-                            {car.int_color}
-                            <img src={car.images[0].url}/>
+                        <div className='each-car-outerbox' key={`${i}`}>
+                            <div className='each-car-zip'>
+                                <p className='each-car-zip-txt'>
+                                    {car.zip}
+                                </p>
+                            </div>
+                            <div className='each-car-img-div'>
+                                <img className='each-car-img' src={car.images[0].url} alt='each-car-img-alt'/>
+                            </div>
+                            <div className='each-car-details-div'>
+                                <p className='each-car-detail'>
+                                    {car.make} {car.model}
+                                </p>
+                                <p className='each-car-detail'>
+                                    {car.price}
+                                </p>
+                                <p className='each-car-detail'>
+                                    {car.mileage}
+                                </p>
+                                <p className='each-car-detail'>
+                                    {car.ext_color}
+                                </p>
+                                <Link>
+                                    View Vehicle Details
+                                </Link>
+                            </div>
                         </div>
                     ))}
                 </div>
